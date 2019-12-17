c54e7e3566f4e729a8ffff6598b6425320ffdef8
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2019-12-17 17:57:32 -0300

    Incluido acesso ao webservice. Precisa correções.

---
+++ EstudoLoginLDAP/Suporte/WebService.swift
@@ -0,0 +1,55 @@
+//
+//  WebService.swift
+//  EstudoLoginLDAP
+//
+//  Created by Roberto Edgar Geiss on 17/12/19.
+//  Copyright © 2019 Roberto Edgar Geiss. All rights reserved.
+//
+
+import Foundation
+
+class AcessoWS
+{
+
+let apiKey = "5a55c163db824179819ef6d1b167c342"
+let url = URL(string: "https://desenv-ws.novohamburgo.rs.gov.br/index.php/api/Memo/buscaSecretarias/")!
+var request = URLRequest(url: url)
+
+request.httpMethod = "GET"
+request.setValue(apiKey, forHTTPHeaderField: "x-api-key")
+request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+let session = URLSession(configuration: URLSessionConfiguration.default)
+//let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
+
+let task = session.dataTask(with: request, completionHandler: { data, response, error in
+
+    if error != nil || data == nil
+    {
+        print("Client error!")
+        return
+    }
+
+    guard let response1 = response as? HTTPURLResponse, (200...299).contains(response1.statusCode)
+    else
+    {
+        print("Server error!");print(response!)
+        return
+    }
+
+    guard let mime = response1.mimeType, mime == "application/json"
+    else
+    {
+        print("Wrong MIME type!")
+        return
+    }
+
+    do {
+        let json = try JSONSerialization.jsonObject(with: data!, options: [])
+        print(json)
+    } catch {
+        print("JSON error: \(error.localizedDescription)")
+    }
+}
+)
+task.resume()
+}
