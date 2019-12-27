d87ec37d61013c44bea7b567b4012e435d5c5bd7
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2019-12-27 13:50:58 -0300

    Ajuste ?

---
+++ EstudoLoginLDAP/Views/PerfilVC.swift
@@ -0,0 +1,18 @@
+//
+//  PerfilVCViewController.swift
+//  EstudoLoginLDAP
+//
+//  Created by Roberto Edgar Geiss on 27/12/19.
+//  Copyright © 2019 Roberto Edgar Geiss. All rights reserved.
+//
+
+import UIKit
+
+class PerfilVC: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
---
+++ EstudoLoginLDAP/Views/SideMenuTVC.swift
@@ -0,0 +1,47 @@
+//
+//  MenuTableViewController.swift
+//  EstudoLoginLDAP
+//
+//  Created by Roberto Edgar Geiss on 26/12/19.
+//  Copyright © 2019 Roberto Edgar Geiss. All rights reserved.
+//
+
+import UIKit
+
+enum MenuType: Int
+{
+    case home
+    case perfil
+    case logout
+    case ajuda
+}
+
+class SideMenuTVC: UITableViewController
+{
+    var didTapMenuType: ((MenuType) -> Void)?
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 4
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        guard let menuType = MenuType(rawValue: indexPath.row) else { return }
+        dismiss(animated: true) { [weak self] in
+            print("Dismissing: \(menuType)")
+            self?.didTapMenuType?(menuType)
+        }
+    }
+}
